b3ba727a1e897919c5fdc33ea73df56992064e03
Jean-Simon Desjardins <jsdesjardinssds@gmail.com>
2016-12-14 21:07:58 -0500

    Update sfc.sh

--- bin/sfc.sh
+++ bin/sfc.sh
@@ -7,11 +7,46 @@
 # Declaring Colors
 
 
+
 # Changing Directory (assuming it is in the Downloads folder)
 cd "$HOME/Downloads"
 
+# Tell user which app he is running
+function appname {
 
-# Asking for file extension type
+echo
+echo -e "Source file compiler by Jean-Simon Desjardins (http://www.github.com/jsgarden)"
+	}
+
+# Ask if build-essential package is installed. If not, install it
+function buildpack {
+
+echo
+echo
+echo -e "Do you have the build-essential package installed ? (Y/N) If unsure, yes."
+read bldpk
+sleep 1
+
+if [ "$bldpk" == y ] || [ "$bldpk" == Y ]
+	then
+		echo
+		echo
+		echo -e "Installing..."
+		sleep 1
+		sudo apt-get install build-essential
+		echo
+		echo -e "Done !"
+		filetype
+
+elif [ "$bldpk" == n ] || [ "bldpk" == N ]
+	then
+		echo
+		echo
+		filetype
+		
+		
+		
+# Ask for file extension type
 function filetype {
 
 echo -e "File extension type ? (.xy.z)"
@@ -28,13 +63,13 @@ echo -e "Are you sure ? (Y/N)"
 echo
 read yn_flnm
 
-if [ "$yn_flnm" == "y" ] || [ "yn_flnm" == "Y" ];
+if [ "$yn_flnm" == "y" ] || [ "$yn_flnm" == "Y" ];
     then
         extr
 
-elif [ "yn_flnm" == "n" ] || [ "yn_flnm" == "N" ];
+elif [ "$yn_flnm" == "n" ] || [ "$yn_flnm" == "N" ];
     then
-        restart
+        exit
 
 
 # Start the extraction commands
@@ -42,9 +77,46 @@ function extr {
 
 echo -e "Starting extraction..."
 echo
+sleep 1
 
-if [ "fltp" == "tar.gz"]
+if [ "$fltp" == ".tar.gz"]
     then
-        #do command to extract .tar.gz files
+        tar -xzvf $flnm$fltp
+        movedir
         
-elif
+elif [ "$fltp" == ".tar.bz2" ]
+	then
+		tar -xjvf $flnm$fltp
+		movedir
+		
+fi }
+
+
+# Change current directory to the extracted one
+function movedir {
+
+echo -e "Displaying current folders..."
+sleep 1
+echo
+ls
+echo
+echo -e "Please enter the name of the newly extracted folder:"
+read foldernm
+echo
+echo
+
+echo -e "Changing current directory..."
+sleep 1
+cd $HOME/Downloads/$foldernm
+echo
+echo -e "Done !"
+
+conf }
+
+
+# Start the ./configure command
+function conf {
+
+echo
+echo -e "Configuring..."
+sleep 1
